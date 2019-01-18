ac6ce465281fb4fcbc5fbbeed81fab0504e6617d
Felix Stumvoll <felix.stumvoll@gmail.com>
2019-01-19 00:14:57 +0100

    changed apiUrl back

--- src/common/apiUrl.js
+++ src/common/apiUrl.js
@@ -1,1 +1,1 @@
-export default 'http://localhost:8000/api'; /*'https://sheltered-journey-72427.herokuapp.com/api';*/
+export default 'https://sheltered-journey-72427.herokuapp.com/api'; /*'http://localhost:8000/api';*/
