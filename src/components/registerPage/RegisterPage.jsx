dfb05a92b08e9c40bbda70c8a3cf16d34f7ed8d9
Felix Stumvoll <felix.stumvoll@gmail.com>
2019-01-20 23:58:49 +0100

    added RegisterPage added autoresizetextbox dependency

---
+++ src/components/registerPage/RegisterPage.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import styled from 'styled-components';
+
+const RegisterPanel = styled.form`
+    display: grid;
+    grid-template-areas:
+        'Label Label'
+        'UsernameLabel Username'
+        'PasswordLabel Password'
+        'PasswordRepeatLabel PasswordRepeat'
+        'RegisterButton RegisterButton';
+    grid-template-rows: 70px repeat(3, 30px) 50px;
+    grid-row-gap: 20px;
+    grid-template-columns: 100px 1fr;
+    grid-column-gap: 10px;
+    font-family: ${({ theme }) => theme.defaultFont};
+    transition: ${({ theme }) => theme.transitionDuration};
+`;
+
+export class RegisterPage extends Component {
+    render() {
+        return <div />;
+    }
+}
+
+const mapStateToProps = state => ({});
+
+const mapDispatchToProps = {};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(RegisterPage);
