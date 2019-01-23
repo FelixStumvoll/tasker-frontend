c6972afac6eec7a78c98c4c1d23b65165d954e43
Felix Stumvoll <felix.stumvoll@gmail.com>
2019-01-23 20:17:44 +0100

    fixed navbar for good

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -16,7 +16,7 @@ const Nav = styled.nav`
     height: ${props => props.theme.navHeight};
     background-color: ${props => props.theme.navColor};
     display: grid;
-    grid-template-columns: 50px 100px minmax(200px, 20vw) 2fr 150px 60px;
+    grid-template-columns: 50px 100px minmax(250px, 400px) 2fr 150px 60px;
     font-family: ${({ theme }) => theme.defaultFont};
     grid-template-areas: 'BackButton . SearchArea . Name Logout';
     position: fixed;
